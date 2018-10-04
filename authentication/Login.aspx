34aa88fea040aa2a8251c746d9696c7b04cfdc8c
PrzemoS <przemeks13@gmail.com>
2018-10-05 01:46:10 +0200

    przyciski logowania reagują. Brak połączenia z BD

--- authentication/Login.aspx
+++ authentication/Login.aspx
@@ -1,5 +1,7 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="authentication.Login" %>
 
+<%@ Import Namespace="System.Web.Security" %>
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
--- authentication/Login.aspx.cs
+++ authentication/Login.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Diagnostics;
 
 namespace authentication
 {
@@ -11,18 +13,24 @@ namespace authentication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
-
+            Membership.CreateUser("root", "root");
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-
+            if (Membership.ValidateUser(tbxLogin.Text, tbxPassword.Text))
+            {
+                System.Diagnostics.Trace.WriteLine("Zalogowano" + tbxLogin.Text);
+            }
+                //FormsAuthentication.RedirectFromLoginPage(tbxLogin.Text, true);
+            else
+                System.Diagnostics.Trace.WriteLine("Nie zalogowano" + tbxLogin.Text);
         }
     }
 }
